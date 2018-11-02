0bfe05ac5098adb26a03ebb24326a23c33cdfa79
Igor Kozlov <i.kozlov@tinkoff.ru>
2018-11-02 16:55:58 +0500

    first commit

---
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -0,0 +1,14 @@
+package fintech.homework07
+
+/**
+  * Реализовать алгоритмы quick-sort и merge-sort
+  *  использую *подходящие* *мутабельные* коллекции
+  */
+
+object Sorting {
+
+//  def mergeSort(???)
+//
+//  def quickSort(???)
+
+}
---
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -0,0 +1,6 @@
+package fintech.homework07
+import org.scalatest.{FlatSpec, Matchers}
+
+class SortingSpec extends FlatSpec with Matchers {
+  ???
+}
