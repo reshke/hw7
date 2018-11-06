eeabe59e8d1befbe0f56ce3e8b9babce2c63acad
reshke <rocklobster2@mail.ru>
2018-11-06 21:21:14 +0500

    QuickSort implemented

--- src/main/scala/fintech/homework07/Sorting.scala
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -9,6 +9,21 @@ import scala.collection.mutable.ListBuffer
 
 object Sorting {
 
+  private def quickSort[T](list: ListBuffer[T])(implicit ord: Ordering[T]): ListBuffer[T] = {
+    def partition(list: ListBuffer[T], pivot: T): (ListBuffer[T], ListBuffer[T]) = {
+      val left = list.filter(item => ord.compare(item, pivot) < 0)
+      val right = list.filter(item => ord.compare(item, pivot) >= 0)
+      (left, right)
+    }
+
+    if (list.length < 2)
+      list
+    else {
+      val (left, right) = partition(list.tail, list.head)
+      (quickSort(left) :+ list.head) ++ quickSort(right)
+    }
+  }
+
   private def mergeSort[T](list : ListBuffer[T])(implicit ord : Ordering[T]) : ListBuffer[T] = {
     def merge(left: ListBuffer[T], right: ListBuffer[T]): ListBuffer[T] = {
       (left.isEmpty, right.isEmpty) match {
@@ -52,7 +67,22 @@ object Sorting {
 
     result
   }
-//
-//  def quickSort(???)
 
+  def quickSort[T](iterable : Seq[T])(implicit ord : Ordering[T]): Seq[T] = {
+    var list = new ListBuffer[T]()
+    var result: Seq[T] = iterable
+
+    for (item <- iterable) {
+      list += item
+      result = result.tail
+    }
+
+    list = quickSort(list)
+
+    for (item <- list) {
+      result = result :+ item
+    }
+
+    result
+  }
 }
--- src/test/scala/fintech/homework07/SortingSpec.scala
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -2,15 +2,22 @@ package fintech.homework07
 import org.scalatest.{FlatSpec, Matchers}
 import fintech.homework07.Sorting._
 import scala.collection.mutable.IndexedSeq
+import scala.collection.immutable.LinearSeq
 
 class SortingSpec extends FlatSpec with Matchers {
-  it should "do smth" in {
+  "mergeSort" should "do smth" in {
     val result = mergeSort(Seq(1 , 2 ,3))
 
     result should be(Seq(1 ,2 , 3))
   }
 
-  it should "sort correctly" in {
+  "quickSort" should "do smth" in {
+    val result = quickSort(Seq(1 , 2 ,3))
+
+    result should be(Seq(1 ,2 , 3))
+  }
+
+  "mergeSort" should "sort correctly" in {
     val result = mergeSort(Seq(3, 2, 1))
     val resultSecond = mergeSort(Seq(2, 1, 3))
     val correctSortedSeq = Seq(1 ,2 , 3)
@@ -19,15 +26,37 @@ class SortingSpec extends FlatSpec with Matchers {
     resultSecond should be(correctSortedSeq)
   }
 
-  it should "sort correctly T <: Seq" in {
+  "quickSort" should "sort correctly" in {
+    val result = quickSort(Seq(3, 2, 1))
+    val resultSecond = quickSort(Seq(2, 1, 3))
+    val correctSortedSeq = Seq(1 ,2 , 3)
+
+    result should be(correctSortedSeq)
+    resultSecond should be(correctSortedSeq)
+  }
+
+  "mergeSort" should "sort correctly T <: Seq" in {
     val result = mergeSort(List(3, 2, 1))
-    val resultSecond = mergeSort(IndexedSeq(2, 3, 1, 4) )
+    val resultSecond = mergeSort(IndexedSeq(2, 3, 1, 4))
+    val resultThird = mergeSort(LinearSeq(8, 6, 7, 2, 2, 1, 1, 0, 67))
 
     result should be(List(1, 2, 3))
     resultSecond should be(IndexedSeq(1, 2, 3, 4))
+    resultThird should be(LinearSeq(0, 1, 1, 2, 2, 6, 7, 8, 67))
   }
 
-  it should "sort with implicit ordering well" in {
+
+  "quickSort" should "sort correctly T <: Seq" in {
+    val result = quickSort(List(3, 2, 1))
+    val resultSecond = quickSort(IndexedSeq(2, 3, 1, 4))
+    val resultThird = quickSort(LinearSeq(8, 6, 7, 2, 2, 1, 1, 0, 67))
+
+    result should be(List(1, 2, 3))
+    resultSecond should be(IndexedSeq(1, 2, 3, 4))
+    resultThird should be(LinearSeq(0, 1, 1, 2, 2, 6, 7, 8, 67))
+  }
+
+  "mergeSort" should "sort with implicit ordering well" in {
     implicit val intOrdering = new Ordering[Int] {
       override def compare(x: Int, y: Int): Int = if (x < y) 1 else if (x == y) 0 else -1
     }
@@ -36,12 +65,32 @@ class SortingSpec extends FlatSpec with Matchers {
     resultSecond should be(IndexedSeq(4, 3, 2, 1))
   }
 
-  it should "sort with implicit ordering well, some another case" in {
+
+  "quickSort" should "sort with implicit ordering well" in {
+    implicit val intOrdering = new Ordering[Int] {
+      override def compare(x: Int, y: Int): Int = if (x < y) 1 else if (x == y) 0 else -1
+    }
+    val resultSecond = quickSort(IndexedSeq(2, 3, 1, 4))
+
+    resultSecond should be(IndexedSeq(4, 3, 2, 1))
+  }
+
+  "mergeSort" should "sort with implicit ordering well, some another case" in {
     implicit val intOrdering = new Ordering[Int] {
       override def compare(x: Int, y: Int): Int = if (x % 5 > y % 5) 1 else if (x % 5 == y % 5) 0 else -1
     }
     val resultSecond = mergeSort(IndexedSeq(2, 3, 1, 4, 5, 6, 7, 9, 8))
 
     resultSecond should be(IndexedSeq(5, 1, 6, 2, 7, 3, 8, 4, 9)) // сохраняет порядок
+  }
+
+
+  "quickSort" should "sort with implicit ordering well, some another case" in {
+    implicit val intOrdering = new Ordering[Int] {
+      override def compare(x: Int, y: Int): Int = if (x % 5 > y % 5) 1 else if (x % 5 == y % 5) 0 else -1
+    }
+    val resultSecond = quickSort(IndexedSeq(2, 3, 1, 4, 5, 6, 7, 9, 8, 3, 3, 3))
+
+    resultSecond should be(IndexedSeq(5, 1, 6, 2, 7, 3, 8, 3, 3, 3, 4, 9)) // тоже сохраняет порядок
   }
 }
