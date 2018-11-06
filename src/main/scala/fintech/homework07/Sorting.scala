168039e567b1d93c42b8b5eb3ba826002614d528
reshke <rocklobster2@mail.ru>
2018-11-06 20:55:10 +0500

    merge sort implemented

--- src/main/scala/fintech/homework07/Sorting.scala
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -1,5 +1,7 @@
 package fintech.homework07
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * Реализовать алгоритмы quick-sort и merge-sort
   *  использую *подходящие* *мутабельные* коллекции
@@ -7,7 +9,49 @@ package fintech.homework07
 
 object Sorting {
 
-//  def mergeSort(???)
+  private def mergeSort[T](list : ListBuffer[T])(implicit ord : Ordering[T]) : ListBuffer[T] = {
+    def merge(left: ListBuffer[T], right: ListBuffer[T]): ListBuffer[T] = {
+      (left.isEmpty, right.isEmpty) match {
+        case (true, true) | (false, true) => left
+        case (true, false) => right
+        case (false, false) =>
+          val x = left.head
+          val y = right.head
+          if (ord.compare(x, y) <= 0) {
+            x +: merge(left.tail, right)
+          }
+          else {
+            y +: merge(left, right.tail)
+          }
+      }
+    }
+
+    if (list.length < 2)
+      list
+    else {
+      val (left, right) = list.splitAt(list.length / 2)
+
+      merge(mergeSort(left), mergeSort(right))
+    }
+  }
+
+  def mergeSort[T](iterable : Seq[T])(implicit ord : Ordering[T]): Seq[T] = {
+    var list = new ListBuffer[T]()
+    var result: Seq[T] = iterable
+
+    for (item <- iterable) {
+      list += item
+      result = result.tail
+    }
+
+    list = mergeSort(list)
+
+    for (item <- list) {
+      result = result :+ item
+    }
+
+    result
+  }
 //
 //  def quickSort(???)
 
--- src/test/scala/fintech/homework07/SortingSpec.scala
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -1,6 +1,47 @@
 package fintech.homework07
 import org.scalatest.{FlatSpec, Matchers}
+import fintech.homework07.Sorting._
+import scala.collection.mutable.IndexedSeq
 
 class SortingSpec extends FlatSpec with Matchers {
-  ???
+  it should "do smth" in {
+    val result = mergeSort(Seq(1 , 2 ,3))
+
+    result should be(Seq(1 ,2 , 3))
+  }
+
+  it should "sort correctly" in {
+    val result = mergeSort(Seq(3, 2, 1))
+    val resultSecond = mergeSort(Seq(2, 1, 3))
+    val correctSortedSeq = Seq(1 ,2 , 3)
+
+    result should be(correctSortedSeq)
+    resultSecond should be(correctSortedSeq)
+  }
+
+  it should "sort correctly T <: Seq" in {
+    val result = mergeSort(List(3, 2, 1))
+    val resultSecond = mergeSort(IndexedSeq(2, 3, 1, 4) )
+
+    result should be(List(1, 2, 3))
+    resultSecond should be(IndexedSeq(1, 2, 3, 4))
+  }
+
+  it should "sort with implicit ordering well" in {
+    implicit val intOrdering = new Ordering[Int] {
+      override def compare(x: Int, y: Int): Int = if (x < y) 1 else if (x == y) 0 else -1
+    }
+    val resultSecond = mergeSort(IndexedSeq(2, 3, 1, 4))
+
+    resultSecond should be(IndexedSeq(4, 3, 2, 1))
+  }
+
+  it should "sort with implicit ordering well, some another case" in {
+    implicit val intOrdering = new Ordering[Int] {
+      override def compare(x: Int, y: Int): Int = if (x % 5 > y % 5) 1 else if (x % 5 == y % 5) 0 else -1
+    }
+    val resultSecond = mergeSort(IndexedSeq(2, 3, 1, 4, 5, 6, 7, 9, 8))
+
+    resultSecond should be(IndexedSeq(5, 1, 6, 2, 7, 3, 8, 4, 9)) // сохраняет порядок
+  }
 }
